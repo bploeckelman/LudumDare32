4fecb9226be221496438965e29731e86634ade20
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2015-04-15 20:57:20 -0500

    Initial commit

---
+++ desktop/src/lando/systems/ld32/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lando.systems.ld32.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld32.LudumDare32;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare32(), config);
+	}
+}
