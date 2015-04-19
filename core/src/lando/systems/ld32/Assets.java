04ad7f167d116f0f9a3eea40c35b2038f9271e56
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2015-04-18 20:16:43 -0500

    Add attack word

--- core/src/lando/systems/ld32/Assets.java
+++ core/src/lando/systems/ld32/Assets.java
@@ -1,21 +1,26 @@
 package lando.systems.ld32;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Assets {
 
-    public static SpriteBatch batch;
+    public static SpriteBatch   batch;
     public static ShapeRenderer shapes;
     public static Texture killphraseBox;
 
+    public static Texture speechBubbleTexture;
+    public static NinePatch speechBubble;
 
     public static void load() {
         batch = new SpriteBatch();
         shapes = new ShapeRenderer();
 
         killphraseBox = new Texture("killphrase-box.png");
+        speechBubbleTexture = new Texture("speech-bubble.png");
+        speechBubble = new NinePatch(speechBubbleTexture, 5, 5, 5, 5);
     }
 
     public static void dispose() {
@@ -23,6 +28,7 @@ public class Assets {
         shapes.dispose();
 
         killphraseBox.dispose();
+        speechBubbleTexture.dispose();
     }
 
 }
---
+++ core/src/lando/systems/ld32/attackwords/AttackWord.java
@@ -0,0 +1,78 @@
+package lando.systems.ld32.attackwords;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import lando.systems.ld32.Assets;
+
+public class AttackWord {
+
+    static final float bubble_margin    = 10f;
+    static final float default_velocity = -60f;
+
+    final String     word;
+    final String[]   letters;
+
+    String typed;
+    final TextBounds textBounds;
+
+    public Vector2    velocity;
+    public BitmapFont font;
+    public Rectangle  bounds;
+
+    public AttackWord(String word, BitmapFont font) {
+        this.word = word.toUpperCase();
+        this.letters = new String[word.length()];
+        for (int i = 0; i < this.word.length(); ++i) {
+            letters[i] = ""+this.word.charAt(i);
+        }
+        this.typed = "";
+        this.font = font;
+        this.textBounds = new TextBounds(font.getBounds(this.word));
+        this.bounds = new Rectangle();
+        this.velocity = new Vector2(default_velocity, 0);
+    }
+
+    public void fire(float x, float y) {
+        typed = "";
+        bounds.set(x, y, textBounds.width + 2 * bubble_margin, textBounds.height + 2 * bubble_margin);
+    }
+
+    public void keyTyped(int keycode) {
+        int i = typed.length();
+        if (i >= letters.length) {
+            return;
+        }
+
+        int letter = Input.Keys.valueOf(letters[i]);
+        if (keycode == letter) {
+            typed += letters[i];
+        }
+    }
+
+    public boolean isComplete() {
+        return typed.equalsIgnoreCase(word);
+    }
+
+    public void update(float delta) {
+        bounds.x += delta * velocity.x;
+        bounds.y += delta * velocity.y;
+    }
+
+    public void render(SpriteBatch batch) {
+        Assets.speechBubble.draw(batch, bounds.x, bounds.y, bounds.width, bounds.height);
+
+        float textX = bounds.x + bounds.width  / 2f - textBounds.width  / 2f;
+        float textY = bounds.y + bounds.height / 2f + textBounds.height / 2f;
+        font.draw(batch, word, textX, textY);
+
+        font.setColor(Color.RED);
+        font.draw(batch, typed, textX, textY);
+        font.setColor(Color.WHITE);
+    }
+
+}
