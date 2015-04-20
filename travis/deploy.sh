a65625a2d40d321d306eb5918575e596bcc19e82
Colin Kennedy <moshen.colin@gmail.com>
2015-04-20 13:51:06 -0500

    Add deploy script for html, jar

---
+++ travis/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir deploy
+cp -r html/build/dist/ deploy/
+cp desktop/build/libs/desktop-*.jar deploy/LudumDare32.jar
+cd deploy
+tar -czf ../LudumDare32.tar.gz ./
+cd ..
+curl -F filedata=@LudumDare32.tar.gz -H "$MOSHEN_UPLOAD_KEY" http://moshen.net/LudumDare32/deploy/
